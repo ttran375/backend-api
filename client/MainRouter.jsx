54a284854d42b81a5f66026b6b9fbb2dab7153c0
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-15 15:34:26 +0000

    Add solution codes

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './core/Home' 
-const MainRouter = () => {
-return ( <div> 
-<Routes>
-<Route exact path="/" element={<Home />}/> 
+import Users from './user/Users.jsx'
+import Signup from './user/Signup.jsx'
+import Signin from './lib/Signin.jsx'
+import Profile from './user/Profile.jsx'
+import PrivateRoute from './lib/PrivateRoute.jsx'
+import EditProfile from './user/EditProfile.jsx'
+
+import Menu from './core/Menu' 
+function MainRouter() {
+return (
+<div>
+ <Menu/>
+
+ 
+ <Routes>
+ <Route path="/" element={<Home />} /> 
+ <Route path="/users" element={<Users />} />
+ <Route path="/signup" element={<Signup />} />
+ <Route path="/signin" element={<Signin />} />
+ <Route
+ path="/user/edit/:userId"
+ element={
+ <PrivateRoute>
+ <EditProfile />
+ </PrivateRoute>
+ }
+ />
+ <Route path="/user/:userId" element={<Profile />} />
 </Routes>
-</div> 
-)
+</div>
+ );
 }
+
 export default MainRouter
